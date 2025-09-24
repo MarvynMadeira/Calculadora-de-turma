f2a86734abc58a5c8ee272ea7b54f69bded2149d
Marvyn Souza Madeira <marvynmadeira@gmail.com>
2025-09-24 11:44:38 -0300

    Commit: "New test carried out"

--- src/test/java/CalculaTeste2.java
+++ src/test/java/CalculaTeste2.java
@@ -11,6 +11,24 @@ class CalculadoraServiceTest {
         //Resultado esperado: 2.0m² por aluno.
 
         assertEquals(2.0, CalculaTurmas.calcularMedia(50.0, 25), 0.001);
-        
+
+    }
+
+    @Test
+    void testCalcularComZero() {
+        //Cenário: Tentar calcular com 0 alunos.
+        // Resultado esperado: Lançar um erro.
+        assertThrows(IllegalArgumentException.class, () -> {
+            CalculaTurmas.calcularMedia(50.0, 0);
+        });
+    }
+
+    @Test
+    void testCalcularComNegativo() {
+        //Cenário: Tentar calcular com -5 alunos.
+        //Resultado esperado: lançar um erro.
+        assertThrows(IllegalArgumentException.class, () -> {
+            CalculaTurmas.calcularMedia(50.0, -5);
+        });
     }
 }
